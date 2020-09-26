f822c04b6616acf9e48c6f41f43cbfd793c926f2
Franciel Leandro <francielleandro@gmail.com>
2020-09-26 08:39:07 -0300

    limpando projeto inicial e criando rotas

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+/*
+Sempre tipar o componente com React.FC
+*/
+const Repository:React.FC = () => {
+
+    return <h1>Repository</h1>;
+};
+
+export default Repository;
