6592a2530c3edac0280eb85de3e74b699450b9bf
Franciel Leandro <francielleandro@gmail.com>
2020-09-27 07:27:09 -0300

    inserindo css e componentes

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import logoSvg from '../../assets/logo.svg';
+
+import {Title, Form} from './styles';
+
 /*
 Sempre tipar o componente com React.FC
 */
 const Dashboard:React.FC = () => {
 
-    return <h1>Dashboard</h1>;
+    return (
+        <>
+            <img src={logoSvg} alt="Github Explorer"/>
+            <Title>Explore repositórios no Github</Title>
+
+            <Form>
+                <input placeholder="Digite o nome do repositório"/>
+                <button type="submit">Pesquisar</button> 
+
+            </Form>
+        </>
+    );
 };
 
 export default Dashboard;
